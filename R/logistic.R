543e7ea72a08376f28fc5d06e3a467449627415d
Rob Hyndman <robjhyndman@gmail.com>
2023-12-01 09:05:06 +1100

    Innovation residuals for logistic are now deviance residuals

--- R/logistic.R
+++ R/logistic.R
@@ -116,9 +116,44 @@ fitted.LOGISTIC <- function(object, ...) {
 #'   residuals()
 #' @export
 residuals.LOGISTIC <- function(object,
-  type = c("deviance", "innovation", "pearson", "working", "response", "partial"), ...)
-{
-  return(object$residuals)
+    type = c("deviance", "innovation", "pearson", "working", "response", "partial"), 
+    ...) {
+  type <- match.arg(type)
+  if (type == "innovation") {
+    type <- "deviance"
+  }
+  y <- object$y
+  r <- object$residuals
+  mu <- object$fitted.values
+  wts <- object$prior.weights
+  switch(type,
+    deviance = ,
+    pearson = ,
+    response = if (is.null(y)) {
+      mu.eta <- object$family$mu.eta
+      eta <- object$linear.predictors
+      y <- mu + r * mu.eta(eta)
+    }
+  )
+  res <- switch(type,
+    deviance = if (object$df.residual > 0) {
+      d.res <- sqrt(pmax((object$family$dev.resids)(y, mu, wts), 0))
+      ifelse(y > mu, d.res, -d.res)
+    } else {
+      rep.int(0, length(mu))
+    },
+    pearson = (y - mu) * sqrt(wts) / sqrt(object$family$variance(mu)),
+    working = r,
+    response = y - mu,
+    partial = r
+  )
+  if (!is.null(object$na.action)) {
+    res <- naresid(object$na.action, res)
+  }
+  if (type == "partial") {
+    res <- res + predict(object, type = "terms")
+  }
+  res
 }
 
 #' Glance a LOGISTIC
